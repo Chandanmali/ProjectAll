3068b2711b4318a33b650b8075ea5ad0392f9768
Chandan Mali <chandanmali21117@gmail.com>
2026-02-19 03:17:18 +0530

    Chat-app-using-websocket

--- React-Projects/chat-app/Frontend/src/main.jsx
+++ React-Projects/chat-app/Frontend/src/main.jsx
@@ -1,10 +1,7 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './components/Home.jsx'
-import Chat from './components/Chat.jsx'
 import Signup from './Authentication/Signup.jsx'
 import Login from './Authentication/Login.jsx'
 import UserChat from './components/UserChat.jsx'
